a7cfc30bb42c7bde725dd6c6697da34679e310d3
Mihir Lad <mihirlad55@gmail.com>
2020-06-25 00:12:01 -0400

    Update ipc-client.c for testing commands

--- ipc-client.c
+++ ipc-client.c
@@ -7,14 +7,46 @@
 #include <stdlib.h>
 
 #define IPC_MAGIC "DWM-IPC"
-#define IPC_MAGIC_LEN 7 // Not including null char
+#define IPC_MAGIC_LEN 7
+
+enum {
+    IPC_TYPE_RUN_COMMAND = 0,
+    IPC_TYPE_GET_TAGS = 1,
+    IPC_TYPE_SUBSCRIBE = 2
+};
+
+enum {
+    IPC_COMMAND_TAG = 0,
+    IPC_COMMAND_TOGGLE_VIEW = 1,
+    IPC_COMMAND_TOGGLE_TAG = 2,
+    IPC_COMMAND_TAG_MONITOR = 3,
+    IPC_COMMAND_FOCUS_MONITOR = 4,
+    IPC_COMMAND_KILL_CLIENT = 5,
+    IPC_COMMAND_TOGGLE_FLOATING = 6,
+    IPC_COMMAND_SET_MFACT = 7,
+    IPC_COMMAND_SET_LAYOUT = 8,
+    IPC_COMMAND_QUIT = 9
+};
+
+enum {
+    IPC_EVENT_TAG_CHANGE,
+    IPC_EVENT_WINDOW_CHANGE
+};
+
 
 typedef struct dwm_ipc_header {
-    uint8_t magic[IPC_MAGIC_LEN];
-    uint32_t size;
-    uint8_t type;
+  uint8_t magic[IPC_MAGIC_LEN];
+  uint32_t size;
+  uint8_t type;
 } __attribute((packed)) dwm_ipc_header_t;
 
+struct ipc_client {
+  int fd;
+  int subscriptions;
+
+  char *buffer;
+  uint32_t buffer_size;
+};
 
 int
 main(int argc, char *argv[])
@@ -34,8 +66,12 @@ main(int argc, char *argv[])
 
     dwm_ipc_header_t msg_header;
     strncpy(msg_header.magic, IPC_MAGIC, IPC_MAGIC_LEN);
-    msg_header.type = 3;
-    char *msg = "TEST MESSAGE. THIS IS A TEST MESSAGE";
+    msg_header.type = IPC_TYPE_RUN_COMMAND;
+    char *msg =
+      "{\n"
+      "  \"command\": \"toggletag\",\n"
+      "  \"args\": [ 4 ]\n"
+      "}";
     msg_header.size = strlen(msg) + 1;
 
     uint8_t buffer[sizeof(dwm_ipc_header_t) + msg_header.size];
