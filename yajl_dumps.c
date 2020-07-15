c43f3e505a6de4f464f1ef41e64713b8fde55d12
Mihir Lad <mihirlad55@gmail.com>
2020-07-14 22:22:38 -0400

    Fix selected_monitor_change_event dump

--- yajl_dumps.c
+++ yajl_dumps.c
@@ -224,7 +224,7 @@ dump_monitor_change_event(yajl_gen gen, const int last_mon_num,
     const int new_mon_num)
 {
   YMAP(
-    YSTR("monitor_change_event"); YMAP(
+    YSTR("selected_monitor_change_event"); YMAP(
       YSTR("old_monitor_number"); YINT(last_mon_num);
       YSTR("new_monitor_number"); YINT(new_mon_num);
     )
