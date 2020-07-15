949551c364a2011cc52a697d5a565046ee0ce271
Mihir Lad <mihirlad55@gmail.com>
2020-07-14 21:16:11 -0400

    Fix doxygen comments
    The @param comments do not require the type definitions. Also use @return
    consistently.

--- IPCClient.h
+++ IPCClient.h
@@ -6,6 +6,10 @@
 #include <stdio.h>
 
 typedef struct IPCClient IPCClient;
+/**
+ * This structure contains the details of an IPC Client and pointers for a linked
+ * list
+ */
 struct IPCClient {
   int fd;
   int subscriptions;
@@ -24,33 +28,33 @@ typedef IPCClient* IPCClientList;
  * Allocate memory for new IPCClient with the specified file descriptor and
  * initialize struct.
  *
- * @param int fd File descriptor of IPC client
+ * @param fd File descriptor of IPC client
  *
- * @returns Address to allocated IPCClient struct
+ * @return Address to allocated IPCClient struct
  */
 IPCClient* ipc_client_new(int fd);
 
 /**
  * Add an IPC Client to the specified list
  *
- * @param IPCClientList *list Address of the list to add the client to
- * @param IPCClient *nc Address of the IPCClient
+ * @param list Address of the list to add the client to
+ * @param nc Address of the IPCClient
  */
 void ipc_list_add_client(IPCClientList *list, IPCClient *nc);
 
 /**
  * Remove an IPCClient from the specified list
  *
- * @param IPCClientList *list Address of the list to remove the client from
- * @param IPCClient *c Address of the IPCClient
+ * @param list Address of the list to remove the client from
+ * @param c Address of the IPCClient
  */
 void ipc_list_remove_client(IPCClientList *list, IPCClient *c);
 
 /**
  * Get an IPCClient from the specified IPCClient list
  *
- * @param IPCClientList list List to remove the client from
- * @param int fd File descriptor of the IPCClient
+ * @param list List to remove the client from
+ * @param fd File descriptor of the IPCClient
  */
 IPCClient* ipc_list_get_client(IPCClientList list, int fd);
 
